be22b84a368693ac4cb6b75ed1d191514354adac
dvmbr <dvmbr@daum.net>
2025-12-24 20:50:31 +0900

    feat: update dependencies and integrate Tailwind CSS
    - Added Tailwind CSS and its Vite plugin to package.json.
    - Updated global styles to use Tailwind CSS and imported Pretendard font.
    - Refactored BaseHead component to import new styles.
    - Created BaseLayout component for consistent layout structure.
    - Added Prettier configuration and ignore file for code formatting.
    - Removed old global styles and replaced with new Tailwind-based styles.

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2,
+
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
